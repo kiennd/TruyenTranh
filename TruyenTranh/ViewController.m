6c814997d57bcb8cf22250f0a6066ff6d274b7f5
kiennd <kien.nguyenduc08@gmail.com>
2013-04-25 04:01:06 +0700

    Initial Commit

---
+++ TruyenTranh/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TruyenTranh
+//
+//  Created by KIENND on 4/25/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
